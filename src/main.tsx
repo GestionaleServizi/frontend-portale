cb67f473588eced6ca9a860f10ba05a2a79713da
GestionaleServizi <servizinet.gestionale@gmail.com>
2025-09-09 15:57:45 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,11 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Login from "./pages/Login.tsx";
-import Segnalazione from "./pages/Segnalazione.tsx"; // la tua pagina form
+import Segnalazione from "./pages/Segnalazione.tsx";
 import DashboardAdmin from "./pages/DashboardAdmin.tsx";
 
 import ProtectedRoute from "./components/ProtectedRoute.tsx";
-import AdminRoute from "./components/AdminRoute.tsx";
-
-import "./index.css";
+import "./styles.css"; // o index.css
 
 const router = createBrowserRouter([
   { path: "/login", element: <Login /> },
@@ -25,9 +23,9 @@ const router = createBrowserRouter([
   {
     path: "/admin",
     element: (
-      <AdminRoute>
+      <ProtectedRoute>
         <DashboardAdmin />
-      </AdminRoute>
+      </ProtectedRoute>
     ),
   },
   { path: "*", element: <Login /> },
