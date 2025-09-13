2fdd6eea52c08ff52cb4dfdeafbeee17fb117456
GestionaleServizi <servizinet.gestionale@gmail.com>
2025-09-14 01:11:45 +0200

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,5 @@
 const u = JSON.parse(localStorage.getItem("user") || "null");
 const isAdmin = !!u?.is_admin;
 
-return (
-  <nav>
-    {/* visibile a tutti gli autenticati */}
-    <a href="/segnalazione">Nuova segnalazione</a>
 
-    {/* solo admin */}
-    {isAdmin && <a href="/admin">Dashboard admin</a>}
-  </nav>
-);
 
