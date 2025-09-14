207e1d6f6cb3a40ccb97ffbe6607f077dc9e0b41
GestionaleServizi <servizinet.gestionale@gmail.com>
2025-09-14 23:53:50 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./theme"; // se non lo usi puoi rimuoverlo
 import App from "./App";
-import { AuthProvider } from "./hooks/useAuth";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <AuthProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AuthProvider>
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
