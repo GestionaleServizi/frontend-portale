7072691a00e4a3feb93df13cc3d09ac78a50b3a8
GestionaleServizi <servizinet.gestionale@gmail.com>
2025-09-15 00:06:01 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme"; // ✅ mantieni il tuo tema personalizzato
+import { AuthProvider } from "./hooks/useAuth";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
