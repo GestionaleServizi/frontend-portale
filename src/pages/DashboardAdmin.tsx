8faaf40f31e7ee9eddd2b51479655ded9fd48cf8
GestionaleServizi <servizinet.gestionale@gmail.com>
2025-09-16 18:16:01 +0200

    Update DashboardAdmin.tsx

--- src/pages/DashboardAdmin.tsx
+++ src/pages/DashboardAdmin.tsx
@@ -121,7 +121,7 @@ export default function DashboardAdmin() {
 
       {/* Logo al centro */}
       <Flex justify="center" mb={6}>
-        <Image src="/logo.png" alt="Logo" boxSize="180px" objectFit="contain" />
+        <Image src="/servizinet_logo.png" alt="Logo" boxSize="180px" objectFit="contain" />
       </Flex>
 
       {/* KPI in card sotto al logo */}
